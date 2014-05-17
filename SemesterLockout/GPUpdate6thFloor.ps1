693cff53801d15ab75c7efa19d3a0f0e7bf0bda2
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-05-16 22:26:22 -0400

    Updated to include configuration manager and Lockout scripts

---
+++ SemesterLockout/GPUpdate6thFloor.ps1
@@ -0,0 +1,2 @@
+﻿$Batch =  Get-ADComputer -Filter * -Properties Name -SearchBase "OU=SOA 6th Floor Studio Workstations,OU=Lab Computers,OU=Architecture,DC=yu,DC=yale,DC=edu" | select name
+Invoke-Command -ComputerName $Batch.name { gpupdate /force}
