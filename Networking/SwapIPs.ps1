885b6c7097c1ad2d447c5fa527d9d4ef9b79f50f
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-06-17 13:20:09 -0400

    Added a script for migrating IPs and started working on a DCS for our servers and client systems to expedite redeployment. Both WIP

---
+++ Networking/SwapIPs.ps1
@@ -0,0 +1,18 @@
+﻿#Wrote this to move an IP address from one NIC to another NIC, without having to install java and roll back IE just to use the DRAC lights out management of my server.
+pause
+
+Set-NetIPInterface -InterfaceAlias "Ethernet" -Dhcp
+
+##Requires Confirmation, will have to look into it
+##http://technet.microsoft.com/en-us/library/jj130903.aspx
+Disable-NetAdapter -Name "Ethernet"
+
+##Requires Confirmation, will have to look into it
+##http://technet.microsoft.com/en-us/library/jj130903.aspx
+Enable-NetAdapter -Name "Ethernet 2"
+
+New-NetIPAddress –InterfaceAlias “Ethernet 2” –IPv4Address “ip address” –PrefixLength 24 -DefaultGateway 172.29.106.1
+
+Set-DnsClientServerAddress -InterfaceAlias “Ethernet 2” -ServerAddresses 130.132.1.10, 130.132.1.11
+
+pause
