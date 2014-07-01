fa90f37f28d163d5c0153494e8ba872e810f6ac6
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-07-01 15:35:28 -0400

    added YSOA Repo

--- YSOA Automation/DCS Configurations/README.txt
+++
@@ -1,1 +0,0 @@
-This section has the varying scripts that generate MOF files used to manage operating systems through PowerShell DCS.
--- YSOA Automation/GetComputerNames/AllComputers.ps1
+++
@@ -1,8 +0,0 @@
-﻿$computerList = .\4thFloorPCs.ps1
-$computerList2 = .\5thFloorPCs.ps1
-$computerList = $computerList + $computerList2
-$computerList2 = .\6thFloorPCs.ps1
-$computerList = $computerList + $computerList2
-$computerList2 = .\7thFloorPCs.ps1
-$computerList = $computerList + $computerList2
-$computerList
