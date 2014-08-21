d3ca9190ffda3eb8ae60fceeb04702f1efe0c8cd
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-08-21 18:08:03 -0400

    created script for USMT earlier, then made the contract generator for some practice.

---
+++ ContactGenerator.ps1
@@ -0,0 +1,51 @@
+﻿#REQUIRES -Version 4.0
+#REQUIRES -Module ActiveDirectory
+<#
+.Synopsis
+   This adds some users to the logged in user's outlook Contacts list
+.DESCRIPTION
+   This is a script that finds some users in the active directory and then populates
+   them into the logged in user's outlook. Need RSAT or Active Directory Powershell
+   Credit for the CSV Import script this was based off of:
+   http://itmicah.wordpress.com/2013/11/14/add-contacts-to-outlook-using-powershell-and-a-csv-file/
+.EXAMPLE
+   .\ContactGenerator.ps1 
+.LINK
+    mailto:patrick.mcmorran@yale.edu
+#>
+function Add-Contact {
+    param ($user)
+    $newcontact = $contacts.Items.Add()
+    $newcontact.Email1Address = $user.Email
+    $FirstName = $user.Givenname
+    $LastName = $user.Surname
+    $newcontact.FirstName = $FirstName
+    $newcontact.LastName = $LastName
+    $newcontact.FullName = "$FirstName $LastName"
+    $newcontact.Save()
+}
+
+# Open Outlook and get contactlist
+$outlook = new-object -com Outlook.Application -ea 1
+$contacts = $outlook.session.GetDefaultFolder(10)
+
+#This Snippet just lists all the outlook properties you could populate with the add contact function, depending on what is in the AD.
+"Below is a list of properties the newcontact has."
+$newcontact = $contacts.Items.Add()
+$Props = $newcontact | gm -MemberType property | ?{$_.definition -like 'string*{set}*'}
+$newcontact.Delete()
+$Props | ForEach-Object {$_.Name}
+
+
+#Get AD Users :::
+$users = Get-ADUser -SearchBase 'OU=Sales,DC=test,DC=com' -Filter '*' -Properties *
+
+#If you run this at the command line, it will tell you all the properies in your AD for a user, which you can then match up in
+#the add contact function to make more detailed contacts.
+#Get-ADUser UserID -Properties *
+
+# Add contacts found in AD Query
+foreach ($user in $users) {
+    Add-Contact $user
+}
+
---
+++ Load-USMTState.ps1
@@ -0,0 +1,33 @@
+﻿#REQUIRES -Version 4.0
+#Should also have USMT installed
+<#
+.Synopsis
+   This Copies USMT files onto the target computer, loads the specified store onto the computer
+.DESCRIPTION
+   This is a script that copies the local USMT program files onto the remote computer. It then 
+   remotes into the machine and initiates a USMT load state, applying the previously captured files.
+.EXAMPLE
+   .\Load-USMTState.ps1 -MachineName Arch-PC-623 -USMTStorePath \\fileserver\usmtshare\arch-pc-623\ -USMTDecryptionKey key
+.LINK
+    mailto:patrick.mcmorran@yale.edu
+#>
+
+param(
+   [Parameter(Mandatory=$TRUE, Position=0)] 
+   [string]
+   $MachineName,
+   [Parameter(Mandatory=$TRUE, Position=1)] 
+   [string]
+   $USMTStorePath,
+   [Parameter(Mandatory=$TRUE, Position=2)] 
+   [string]
+   $USMTDecryptionKey
+   )
+
+Copy-Item "C:\Program Files (x86)\Windows Kits\8.1\Assessment and Deployment Kit\User State Migration Tool\amd64" "\\$MachineName\c$\USMT" -Recurse
+Copy-Item $USMTStorePath "\\$MachineName\c$\USMTStore" -Recurse
+
+Invoke-Command -ComputerName $MachineName -AsJob {
+    Write-Output "Value of Key: $using:USMTDecryptionKey"
+    C:\USMT\loadstate.exe "C:\USMTStore" /decrypt /key:"$using:USMTDecryptionKey"
+}
